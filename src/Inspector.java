927f5d2d335f1bdf75d78006d643ad9c5ec2486f
benton.luu <benton.luu@ucalgary.ca>
2019-11-13 16:31:17 -0700

    Remove superclass, interface and method code from Inspector

--- src/Inspector.java
+++ src/Inspector.java
@@ -13,19 +13,10 @@ public class Inspector {
         // 1) Class name
         printClass(c, depth);
 
-        // 2) Super-class name
-        //printSuperclass(c, obj, recursive, depth);
-
-        // 3) Interface name
-        //printInterfaces(c, obj, recursive, depth);
-
-        // 4) Constructor name, parameter types and modifier
+        // 2) Constructor name, parameter types and modifier
         printConstructor(c, obj, recursive, depth);
 
-        // 5) Method name, exceptions thrown, parameter types, return type, and modifier
-        //printMethods(c, obj, recursive, depth);
-
-        // 6) Field name, type, modifier and current value
+        // 3) Field name, type, modifier and current value
         printFields(c, obj, recursive, depth);
     }
 
@@ -33,36 +24,6 @@ public class Inspector {
     public void printClass(Class c, int depth) {
         formatPrint("CLASS", depth);
         formatPrint("Class: " + c + "\n", depth);
-    }
-
-    // Print superclass information
-    public void printSuperclass(Class c, Object obj, boolean recursive, int depth) {
-        formatPrint("SUPERCLASS (" + c.getName() + ")", depth);
-
-        Class superclass = c.getSuperclass();
-        if (superclass != null) {
-            formatPrint("Superclass: " + superclass + "\n", depth);
-            inspectClass(c.getSuperclass(),obj,recursive, depth + 1);
-        }
-        else {
-            formatPrint("Superclass: null\n", depth);
-        }
-    }
-
-    // Print interface information
-    public void printInterfaces(Class c, Object obj, boolean recursive, int depth) {
-        formatPrint("INTERFACES (" + c.getName() + ")", depth);
-
-        Class[] interfaceList = c.getInterfaces();
-        if (interfaceList.length != 0) {
-            for (Class inter : interfaceList) {
-                formatPrint("Interface: " + inter + "\n", depth);
-                inspectClass(inter,obj,recursive,depth + 1);
-            }
-        }
-        else {
-            formatPrint("No interface(s) found\n", depth);
-        }
     }
 
     // Print constructor information
@@ -89,44 +50,6 @@ public class Inspector {
         }
         else {
             formatPrint("No constructor(s) found\n", depth);
-        }
-    }
-
-    // Print method information
-    public void printMethods(Class c, Object obj, boolean recursive, int depth) {
-        formatPrint("METHODS (" + c.getName() + ")", depth);
-
-        Method methodList[] = c.getDeclaredMethods();
-        if (methodList.length != 0) {
-            for (Method method : methodList) {
-                formatPrint("Method name: " + method.getName(), depth);
-
-                Class[] exceptionList = method.getExceptionTypes();
-                if (exceptionList.length != 0) {
-                    for (Class exception : exceptionList) {
-                        formatPrint("Method exception: " + exception, depth);
-                    }
-                }
-                else {
-                    formatPrint("Method exception: None", depth);
-                }
-
-                Class[] mParamTypes = method.getParameterTypes();
-                if (mParamTypes.length != 0) {
-                    for (Class paramType : mParamTypes) {
-                        formatPrint("Method parameter type: " + paramType, depth);
-                    }
-                }
-                else {
-                    formatPrint("Method parameter type: None", depth);
-                }
-
-                formatPrint("Method return type: " + method.getReturnType(), depth);
-                formatPrint("Method modifier: " + Modifier.toString(method.getModifiers()) + "\n", depth);
-            }
-        }
-        else {
-            formatPrint("No method(s) found\n", depth);
         }
     }
 
